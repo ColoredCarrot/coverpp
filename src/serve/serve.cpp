afa9495fbaef7c142ae69522f8ab3366190d7872
Julian K <7807570+ColoredCarrot@users.noreply.github.com>
2024-01-17 14:49:19 +0100

    Serve source files

--- src/serve/serve.cpp
+++ src/serve/serve.cpp
@@ -7,16 +7,48 @@
 
 namespace coverpp
 {
+static std::optional<std::string> read_file(const std::filesystem::path& file)
+{
+    std::ifstream ifs(file, std::ios_base::in | std::ios_base::binary | std::ios_base::ate);
+    if (!ifs)
+    {
+        return std::nullopt;
+    }
+
+    const auto size = ifs.tellg();
+    ifs.seekg(0, std::ios::beg);
+
+    std::vector<char> bytes(size);
+    ifs.read(bytes.data(), size);
+
+    return std::string{bytes.data(), static_cast<std::size_t>(size)};
+}
+
 int serve(const ServeOptions& options)
 {
     const std::filesystem::path webapp_path{""};
 
     crow::SimpleApp app;
 
-    CROW_CATCHALL_ROUTE(app)([&] {
+    CROW_ROUTE(app, "/src/<path>")([&](const std::filesystem::path& requested_path) {
+        const auto path = weakly_canonical(requested_path);
+
+        if (!std::filesystem::is_regular_file(path))
+        {
+            return crow::response{404, "text/plain", "404 Not Found"};
+        }
+
+        auto content = read_file(path);
+        if (!content)
+        {
+            return crow::response{404, "text/plain", "404 Not Found"};
+        }
+
         crow::response response;
-        response.set_static_file_info(webapp_path.u8string());
-        return "Foo";
+        response.body = *std::move(content);
+        response.set_header("Content-Length", std::format("{}", response.body.size()));
+        response.set_header("Content-Type", "text/plain");
+        return response;
     });
 
     auto future = app
