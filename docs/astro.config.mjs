66136f938b77ad4610a32f709211085851397b73
Julian K <7807570+coloredcarrot@users.noreply.github.com>
2026-08-26 15:57:00 +0200

    Add docs publishing workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,9 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+    site: process.env.ASTRO_SITE,
+    base: process.env.ASTRO_BASE,
+
     trailingSlash: "never",
     integrations: [
         starlight({
@@ -18,7 +21,7 @@ export default defineConfig({
                     items: [{ autogenerate: { directory: "reference" } }],
                 },
             ],
-            favicon: "/favicon.png",
+            favicon: `${process.env.ASTRO_BASE ?? ""}/favicon.png`,
             editLink: {
                 baseUrl:
                     "https://github.com/ColoredCarrot/coverpp/edit/master/docs/",
