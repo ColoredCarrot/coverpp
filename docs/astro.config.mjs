30c776ce20c353c3b50b76e6b0d7213d5c34d8b4
Julian K <7807570+coloredcarrot@users.noreply.github.com>
2026-08-26 16:28:57 +0200

    Fix docs deployment under path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,61 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
 import starlight from "@astrojs/starlight";
+import { visit } from "unist-util-visit";
+
+/** @import { Root } from "hast" */
+
+/** @type {string|undefined} */
+const base = process.env.ASTRO_BASE || undefined;
+
+/**
+ * @type {() => (tree: Root) => void}
+ */
+const prefixBaseUrls = () => {
+    if (!base) {
+        return () => {};
+    }
+
+    let effectiveBase = base;
+    if (!effectiveBase.startsWith("/")) {
+        effectiveBase = "/" + effectiveBase;
+    }
+    if (effectiveBase.endsWith("/")) {
+        effectiveBase = effectiveBase.slice(0, -1);
+    }
+
+    return ast => {
+        visit(ast, "element", node => {
+            if (node.tagName === "a") {
+                const url = node.properties.href;
+                if (
+                    typeof url === "string" &&
+                    url.startsWith("/") &&
+                    !url.startsWith("//") &&
+                    !url.startsWith(`${effectiveBase}/`)
+                ) {
+                    node.properties.href = effectiveBase + url;
+                }
+            }
+            // TODO also images with src
+        });
+    };
+};
 
 // https://astro.build/config
 export default defineConfig({
     site: process.env.ASTRO_SITE,
-    base: process.env.ASTRO_BASE,
+    base: base,
 
     trailingSlash: "never",
+
+    markdown: {
+        processor: unified({
+            rehypePlugins: [prefixBaseUrls],
+        }),
+    },
+
     integrations: [
         starlight({
             title: "Cover++ Documentation",
@@ -21,7 +69,7 @@ export default defineConfig({
                     items: [{ autogenerate: { directory: "reference" } }],
                 },
             ],
-            favicon: `${process.env.ASTRO_BASE ?? ""}/favicon.png`,
+            favicon: `${base ?? ""}/favicon.png`,
             editLink: {
                 baseUrl:
                     "https://github.com/ColoredCarrot/coverpp/edit/master/docs/",
