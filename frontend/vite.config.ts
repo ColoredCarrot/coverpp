fa1598722674f157051061af94aa8bc19d794131
Julian K <7807570+coloredcarrot@users.noreply.github.com>
2026-09-02 11:05:28 +0200

    Adjust path to report during dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,48 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import fs from "node:fs";
 // import { viteSingleFile } from "vite-plugin-singlefile";
+
+function ensureSameDrive(inputPath: string): void {
+    if (process.platform !== "win32") {
+        return;
+    }
+
+    const inputDrive = path.win32
+        .parse(inputPath)
+        .root.match(/^[A-Za-z]:/)?.[0];
+    if (!inputDrive) {
+        return;
+    }
+
+    const cwdDrive = path.win32
+        .parse(process.cwd())
+        .root.match(/^[A-Za-z]:/)?.[0];
+    if (!cwdDrive) {
+        return;
+    }
+
+    if (inputDrive.toLowerCase() !== cwdDrive.toLowerCase()) {
+        throw new Error(
+            `Vite's /@fs/ handling only supports files on the same drive as the current working directory (${cwdDrive}), but the report path is on ${inputDrive} `,
+        );
+    }
+}
+
+function getPathToDevReport() {
+    const file = path.resolve(__dirname, "../cmake-build-debug/report.coverpp");
+
+    if (!fs.existsSync(file)) {
+        throw new Error(
+            `Cover++ report file for development not found: ${file}`,
+        );
+    }
+
+    ensureSameDrive(file);
+
+    return `String.raw\`${file}\``;
+}
 
 export default defineConfig(({ mode }) => ({
     plugins: [
@@ -24,8 +65,7 @@ export default defineConfig(({ mode }) => ({
             "Cross-Origin-Embedder-Policy": "require-corp",
         },
         fs: {
-            // Allow access to the Cover++ project directory
-            allow: [".."],
+            strict: mode !== "development",
         },
         // Only listen on localhost; don't expose ourselves to the outside world
         host: "localhost",
@@ -34,8 +74,6 @@ export default defineConfig(({ mode }) => ({
         // If running under a dev server, there is no Cover++ server behind us, so we cannot access /ctx
         // Instead, inject the absolute path to an example report file
         __DEV_ABSOLUTE_PATH__:
-            mode === "development"
-                ? `String.raw\`${path.resolve(__dirname, "../cmake-build-debug-visual-studio/Debug/coverpp-report/report.coverpp")}\``
-                : undefined,
+            mode === "development" ? getPathToDevReport() : undefined,
     },
 }));
